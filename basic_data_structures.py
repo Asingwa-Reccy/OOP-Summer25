88bb2221f0fe3ef60308bdd50c20de90de98b0c3
Asingwa-Reccy <recyasingwa@gmail.com>
2025-03-13 13:22:12 +0100

    basic_data_structures.py

--- basic_data_structures.py
+++ basic_data_structures.py
@@ -1,1 +1,13 @@
+name = "Reccy Asingwa"
+age = 26
+city = "Nairobi, Kenya"
+university = "University of Arts and Technology in Warsaw"
 
+print("Personal Information:")
+print("Name:", name)
+print("Age:", age, "years old")
+print("City:", city)
+print("University:", university)
+
+next_year_age = age + 1
+print("Next year, you will be", next_year_age, "years old.")
