d97f3fba8f76bb8c749e6cba25d17feb89e4427a
Asingwa-Reccy <recyasingwa@gmail.com>
2025-03-13 09:58:03 +0000

    Add new file

---
+++ student_details.py
@@ -0,0 +1,14 @@
+# Student Data Variables
+firts_namae = "John"
+last_name = "Doe"
+index_number = "35457" 
+nationality = "Polish"
+starting_date = "2025-03-13"
+courses =  ("Mathematics" "Phisics" "Object Programing")
+
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Index Number: {index number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses:"Mathematics","Phisics", "Object Programing".join (courses))
