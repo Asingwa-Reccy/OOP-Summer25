5e25ba889a2f854436b16851904cd4ac50d6a8b8
Asingwa-Reccy <recyasingwa@gmail.com>
2025-03-13 12:18:05 +0000

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,16 @@
+# Defining variables
+name = "Reccy Asingwa"
+age = 26
+city = "Nairobi, Kenya"
+university = "University of Arts and Technology in Warsaw"
+
+# Displaying the information
+print("Personal Information:")
+print("Name:", name)
+print("Age:", age, "years old")
+print("City:", city)
+print("University:", university)
+
+# Using an integer function
+next_year_age = age + 1
+print("Next year, you will be", next_year_age, "years old.")
