5524d910ebf80b36c5b32622639247deefbcdc99
Asingwa-Reccy <recyasingwa@gmail.com>
2025-04-03 09:19:28 +0000

    Add new file

---
+++ w3_school./python_variables.py
@@ -0,0 +1,24 @@
+x = 5
+y = "recy"
+print(x)
+print(y)
+
+#i will create instagram account data
+username = "little_cake_girl"
+followers = 3000
+bio = "Baker | Traveller | Lifeist"
+verified = "true"
+print(username)
+print(followers)
+print(bio)
+print(bio)
+
+#post varriables
+reel = "just hit 20k"
+likes = 5000
+reposts = 40
+coments = 22
+print (reel)
+print (likes)
+print (reposts)
+print (coments)
