9994a063a266e9639fce4fe0418e51bee6628b9b
Asingwa-Reccy <recyasingwa@gmail.com>
2025-04-08 11:42:42 +0000

    Add new file

--- my_assignments.py
+++ my_assignments.py
@@ -1,5 +1,74 @@
-# Create three variables name, age, and country with your own details. Print a sentence using all of them
+# Q1    Create three variables name, age, and country with your own details. Print a sentence using all of them
 x = "my name is reccy"
 y = "i am 77 years"
 z = "and i come from Kenya"
 print(x, y, z)
+
+# Q2    Assign values to x, y, and z in a single line. Make x = 10, y = "hello", z = 3.5.
+x, y, z = 10 , "hello", 3.5
+print (x)
+print(y)
+print(z)
+
+# Q3    Assign the value "apple" to three different variables: fruit1, fruit2, and fruit3, all in one line.
+fruit1 = "apple"
+fruit2 = "apple"
+fruit3 ="apple"
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+# Q5 Create a variable product with value "Shoes" and a variable price with value 49.99. Print
+product = "Shoes" 
+price = 49.99
+print(product)
+print(price)
+
+# Create a global variable called score with value 50. Write a function that adds 10 to score and prints the new value.
+score = 50
+
+def myfunc():
+  score = 50
+  print(score + 10)
+
+myfunc()
+
+print("score " + 10)
+
+# Q6    Enter their favorite color and store it in a variable. Then print a message using that variable.
+
+favorite_color = input("What is your favorite color? ")
+
+
+print(f"My favorite color is {favorite_color}.")
+
+
+ # Assign your first name and age to two variables. Print this sentence using variables:
+ name = "reccy"
+age = 77
+print(f"Hi, my name is {reccy} and I am {77} years old")
+
+# Q7   Store your age in a variable and print a sentence saying "I am [age] years old.
+
+age = 77
+print(f"I am {77} years old.")
+
+# Q8    Create two variables first_name and last_name. Assign your first and last name to them. Then, print your full name by concatenating the two variables.
+
+first_name = "Reccy"
+last_name = "Asingwa"
+
+print(first_name + " " + last_name)
+
+
+
+#Q9     Assign two numbers to variables x and y. Add them together and print the result.
+x = 10
+y = 20
+result = x + y
+print("The result of the addition is:", result)
+
+# Q10   Create a variable favorite_food and assign your favorite food to it. Print a message saying "My favorite food is [favorite_food
+favorite_food = "Pizza"
+
+print(f"My favorite food is {favorite_food}.")
