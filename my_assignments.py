b21888d843ec63f092b7b5307fe3da641d55b525
Asingwa-Reccy <recyasingwa@gmail.com>
2025-04-08 10:03:30 +0000

    Add new file

---
+++ my_assignments.py
@@ -0,0 +1,5 @@
+# Create three variables name, age, and country with your own details. Print a sentence using all of them
+x = "my name is reccy"
+y = "i am 77 years"
+z = "and i come from Kenya"
+print(x, y, z)
