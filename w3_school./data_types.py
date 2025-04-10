29aabf6da7405a1617eb66371d5f3604b08ebca2
Asingwa-Reccy <recyasingwa@gmail.com>
2025-04-10 12:41:30 +0200

    data_types.py

---
+++ w3_school./data_types.py
@@ -0,0 +1,46 @@
+# Creates variables of the following types
+# Integer
+x = 100
+print (x)
+print(type(x))
+
+# Float
+days = 15.4
+print(days)
+print(type(days))
+
+# String
+x = "hello world!"
+print(x)
+print(type(x))
+
+# Boolean
+x = False
+print(x)
+print(type(x))
+
+# Tuple
+continent = ("Africa", "Europe", "America")
+print(continent)
+print(type(continent))
+
+# List
+girls = ["anayah", "jamaiah", "nayla"]
+print(girls)
+print(type(girls))
+
+# Dictionariy
+computers = {"HP": 25, "Lenovo": 15, "Apple" : 20}
+print(computers)
+print(type(computers))
+
+# Set
+fruits = {"mangoes", "bananas", "watermellons"}
+print(fruits)
+print(type(fruits))
+
+# add 2 integers
+a = 5 
+b = 12 
+sum = a+b
+print("The sum is:", sum)
