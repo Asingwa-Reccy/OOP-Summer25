5c2dd961e8f937d5f9e216568664834d1255471e
Asingwa-Reccy <recyasingwa@gmail.com>
2025-05-07 03:05:31 +0000

    Add notebook

---
+++ w3_school./dictionary.py
@@ -0,0 +1,20 @@
+Car1 = {
+    'name':'Toyota R4',
+    'number_plate':'WE 15534'
+}
+print("My car is", Car1['name']) 
+Student1 = {
+    'name':'reccy',
+    'index_number':'11511'
+}
+print("My name is", Student1['name'], Student1['index_number'])
+Fruits1 = {
+    'name':'Kiwis',
+    'tatal_fruits':'1022'
+}
+print("Available fruits in store are",Fruits1['tatal_fruits'], Fruits1['name'])
+Student1={
+    'name':'Purple',
+    'index_number':'11511'
+}
+print("My name is", Student1['name'])
